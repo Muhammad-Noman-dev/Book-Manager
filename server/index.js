c839ee9f0c8922e577aee039d7bd2baec12dff60
Muhammad Noman <nomi.qasim7788@gmail.com>
2026-07-07 17:50:21 +0500

    Update backend CORS configuration

--- server/index.js
+++ server/index.js
@@ -1,25 +1,30 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const dotenv = require("dotenv")
 
 const databaseConnection = require("./database");
-const bookRouter  = require ("./routes/book.routes");
-
+const bookRouter = require("./routes/book.routes");
 
 const app = express();
-app.use(express.json());
-app.use(cors())
 
-dotenv.config();
+app.use(express.json());
+
+app.use(cors({
+  origin: [
+    "https://book-manager-u73i.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 databaseConnection();
 
-app.get("/" , (req , res )=>{
-    res.send("hello home page")
-})
-app.use("/book" ,  bookRouter)
+app.get("/", (req, res) => {
+  res.send("hello home page");
+});
 
+app.use("/book", bookRouter);
 
-app.listen(8000 , ()=>{
-    console.log("Port is running on 8000")
-})
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Port is running on ${process.env.PORT || 8000}`);
+});
